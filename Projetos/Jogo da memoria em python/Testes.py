bf31342e5556ca84485be4eed45ca2a617a7b77a
leonardo-istamilo <leonardoistamilo15@gmail.com>
2023-12-09 15:41:19 -0300

    Criando as cartas com função

--- Projetos/Jogo da memoria em python/Testes.py
+++ Projetos/Jogo da memoria em python/Testes.py
@@ -4,14 +4,12 @@ from random import shuffle
 # Configurações iniciais do jogo:
 NUM_LINHAS = 4
 NUM_COLUNAS = 4
-CORES = ['blue', 'cyan', 'green', 'magenta',
-         'orange', 'purple', 'red', 'yellow']
+CORES = ['blue', 'cyan', 'green', 'magenta', 'orange', 'purple', 'red', 'yellow']
 NUM_TENTATIVAS = 25
 
 # Embaralhando a lista de cores:
 CORES = CORES + CORES  # Duplicando as cores
 shuffle(CORES)  # Embaralhando
-
 
 def centralizar_janela(janela):
     largura_janela = 500
@@ -23,12 +21,8 @@ def centralizar_janela(janela):
 
     janela.geometry(f'{largura_janela}x{altura_janela}+{posicao_x}+{0}')
 
-
 def Compara_Cor(botao, cor):
-    # Aqui você pode realizar a lógica de comparação da cor
-    # Neste exemplo, vou simplesmente mudar a cor do botão para a cor da carta
     botao.configure(fg_color=cor)
-
 
 janela = ctk.CTk()
 janela.title("Jogo da Memória")
@@ -47,10 +41,12 @@ for linha in range(NUM_LINHAS):
             height=120,
             corner_radius=0,
             fg_color="Black",
-            command=lambda index=index: Compara_Cor(
-                botoes[index], str(CORES[index]))
+            command=lambda i=index: Compara_Cor(botoes[i], str(CORES[i]))
         )
         carta.place(x=20 + coluna * 120, y=20 + linha * 140)
         botoes.append(carta)
+
+# Exemplo de como alterar a cor do primeiro botão após 2 segundos
+janela.after(2000, lambda: Compara_Cor(botoes[0], 'red'))
 
 janela.mainloop()
--- Projetos/Jogo da memoria em python/main.py
+++ Projetos/Jogo da memoria em python/main.py
@@ -1,7 +1,5 @@
 import customtkinter as ctk
-import _tkinter
 from random import shuffle
-from time import sleep
 
 # OBS: Criar uma def para dimensionar os wights e, a janela, automaticamente.
 # OBS: Criar uma def para mudar a cor do
@@ -34,19 +32,19 @@ cont = 0
 numero_de_Tentativas = 0
 
 
-def Compara_Cor(nome_carta, cor):
-    nome_carta.configure(fg_color=cor)  # Mostra a cor verdadeira da carta
+def Compara_Cor(carta, cor):
+    carta.configure(fg_color=cor)  # Mostra a cor verdadeira da carta
     # OBS.: Retire o 'global'
     global numero_de_Tentativas, nome_carta1, nome_carta2, cont, cor1, cor2
 
     if cont % 2 == 0:
         cor1 = cor
-        nome_carta1 = nome_carta
+        nome_carta1 = carta
 
         print('cor 1', cor1)
     else:
         cor2 = cor
-        nome_carta2 = nome_carta
+        nome_carta2 = carta
 
         print('cor 2', cor2)
 
@@ -55,10 +53,11 @@ def Compara_Cor(nome_carta, cor):
         else:
             print(f"A cor '{cor1}' é diferente da cor '{cor2}'")
 
-        sleep(2)
         # Retorna as cartas para a cor preto:
-        nome_carta1.configure(fg_color="Black")
-        nome_carta2.configure(fg_color="Black")
+        nome_carta1.after(
+            3000, lambda: nome_carta1.configure(fg_color="Black"))
+        nome_carta2.after(
+            3000, lambda: nome_carta2.configure(fg_color="Black"))
 
         # Conta a quantidade de tentativas
         numero_de_Tentativas += 1
@@ -73,146 +72,25 @@ janela = ctk.CTk()  # Criar janela
 janela.title("Jogo da Memória")
 centralizar_janela(janela)
 
-
-# Criar Cartas
-# Primeira linha
-carta1x1 = ctk.CTkButton(master=janela,
-                         text="CARTA 1",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(carta1x1, str(CORES[0])))
-carta1x1.place(x=20, y=20)
-
-carta1x2 = ctk.CTkButton(master=janela,
-                         text="CARTA 2",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(carta1x2, str(CORES[1])))
-carta1x2.place(x=140, y=20)
-
-carta1x3 = ctk.CTkButton(master=janela,
-                         text="CARTA 3",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(carta1x3, str(CORES[2])))
-carta1x3.place(x=260, y=20)
-
-carta1x4 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(carta1x4, str(CORES[3])))
-carta1x4.place(x=380, y=20)
-
-# Segunda linha:
-carta2x1 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[4]))).place(x=20, y=160)
-
-carta2x2 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[5]))).place(x=140, y=160)
-
-carta2x3 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[6]))).place(x=260, y=160)
-
-carta2x4 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[7]))).place(x=380, y=160)
-
-# Terceira Linha
-carta3x1 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[8]))).place(x=20, y=300)
-
-carta3x2 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[9]))).place(x=140, y=300)
-
-carta3x3 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[10]))).place(x=260, y=300)
-
-carta3x4 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[11]))).place(x=380, y=300)
-
-# Quarta linha
-carta4x1 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[12]))).place(x=20, y=440)
-
-carta4x2 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[13]))).place(x=140, y=440)
-
-carta4x3 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[14]))).place(x=260, y=440)
-
-carta4x4 = ctk.CTkButton(master=janela,
-                         text="",
-                         width=100,
-                         height=120,
-                         corner_radius=0,
-                         fg_color="Black",
-                         command=lambda: Compara_Cor(str(CORES[15]))).place(x=380, y=440)
+# Criando cartas:
+cartas = []
+for linha in range(NUM_LINHAS):
+    for coluna in range(NUM_COLUNAS):
+        # nome_carta = f'carta{linha+1}x{coluna+1}'
+        index = linha * NUM_COLUNAS + coluna
+        carta = ctk.CTkButton(
+            master=janela,
+            text=f"CARTA {index+1}",
+            width=100,
+            height=120,
+            fg_color='black',
+            command=lambda i=index: Compara_Cor(cartas[i], str(CORES[i]))
+        )
+        carta.place(x=20+(coluna*110), y=20+(linha*130))
+        cartas.append(carta)
 
 tentativas = ctk.CTkLabel(
-    janela, text=f"Tentativas: 0/25", font=("arial bold", 16))
-tentativas.place(x=180, y=580)
+    janela, text="Tentativas 0/25", font=('arial bold', 16))
+tentativas.place(x=200, y=580)
 
 janela.mainloop()

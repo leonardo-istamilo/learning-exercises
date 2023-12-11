b94d5d481f6666bbcf26100125faee08f0563f21
leonardo-istamilo <leonardoistamilo15@gmail.com>
2023-12-10 23:16:41 -0300

    Up
    Continua com bugs na quantidade de clicks

--- Projetos/Jogo da memoria em python/Testes.py
+++ Projetos/Jogo da memoria em python/Testes.py
@@ -1,49 +1,56 @@
+from datetime import datetime
+horario_ultimo_click = '21:52:25'
+
+""" click = datetime.today().strftime('%H:%M:%S') #Pega a hora atual
+horario_clicks.append(click) """
+hora_atual = '21:55:30'
+
+def Diferenca_Entre_Horarios(horario, horario_atual):
+    list_horario = horario.split(':')
+    list_horario_atual = horario_atual.split(':')
+
+    horario_HORAS = int(list_horario[0])
+    horario_MINUTOS = int(list_horario[1])
+    horario_SEGUNDOS = int(list_horario[2])
+
+    horario_atual_HORAS = int(list_horario_atual[0])
+    horario_atual_MINUTOS = int(list_horario_atual[1])
+    horario_atual_SEGUNDOS = int(list_horario_atual[2])
+
+    tot_horas = horario_atual_HORAS - horario_HORAS
+    tot_minutos = horario_atual_MINUTOS - horario_MINUTOS
+    tot_segundos = horario_atual_SEGUNDOS - horario_SEGUNDOS
+
+    total_segundos = tot_horas*3600 + tot_minutos*60 + tot_segundos #diferença de segundos entre os horários.
+
+    return total_segundos 
+
+##
 import customtkinter as ctk
-from random import shuffle
-import tkinter
+import time
 
-# Configurações iniciais do jogo:
-NUM_LINHAS = 4
-NUM_COLUNAS = 4
-CORES = ['blue', 'cyan', 'green', 'magenta', 'orange', 'purple', 'red', 'yellow']
-NUM_TENTATIVAS = 25
-
-# Embaralhando a lista de cores:
-CORES = CORES + CORES  # Duplicando as cores
-shuffle(CORES)  # Embaralhando
-
-def centralizar_janela(janela):
-    largura_janela = 500
-    altura_janela = 685
-
-    largura_tela = janela.winfo_screenwidth()
-
-    posicao_x = (largura_tela - largura_janela) // 2
-
-    janela.geometry(f'{largura_janela}x{altura_janela}+{posicao_x}+{0}')
-
-def Compara_Cor(botao, cor):
-    botao.configure(fg_color=cor)
-
-def Oculta_Botao(botao):
-    botao.pack_forget()  # ou botao.grid_forget(), dependendo de como os botões estão organizados
+def clique_no_botao():
+    # Desativa o botão temporariamente
+    botao.configure(state=ctk.DISABLED)
+    
+    # Coloque aqui a lógica que deve ser executada ao clicar no botão
+    print("Botão clicado!")
+    
+    # Simula um atraso de 2 segundos
+    time.sleep(2)
+    
+    # Reativa o botão após o atraso
+    botao.config(state=ctk.NORMAL)
 
 janela = ctk.CTk()
-janela.title("Jogo da Memória")
-centralizar_janela(janela)
+janela.title("Suspender Função do Botão")
 
-b1 = ctk.CTkButton(janela, text='b1')
-b1.pack()
-
-b2 = ctk.CTkButton(janela, text='b2')
-b2.place(x=20, y= 50)
-
-b3 = ctk.CTkButton(janela, text='b3')
-b3.pack()
-
-b4 = ctk.CTkButton(janela, text='b4')
-b4.pack()
-
-b2.place_forget()
+# Crie um botão com a função de clique
+botao = ctk.CTkButton(
+    master=janela,
+    text="Clique-me",
+    command=clique_no_botao
+)
+botao.pack()
 
 janela.mainloop()
--- Projetos/Jogo da memoria em python/main.py
+++ Projetos/Jogo da memoria em python/main.py
@@ -1,8 +1,10 @@
 import customtkinter as ctk
 from random import shuffle
+from time import sleep
+from datetime import datetime
 
 # OBS: Criar uma def para dimensionar os wights e, a janela, automaticamente.
-# OBS: Criar uma def para mudar a cor do
+
 # Melhorias: Adicionar efeitos sonoros e visuais para eliminação de cartas e finalização do jogo.
 
 
@@ -29,56 +31,71 @@ def centralizar_janela(janela):
     janela.geometry(f'{largura_janela}x{altura_janela}+{posicao_x}+{0}')
 
 
+
+
 cont = 1
 numero_de_Tentativas = 0
-
-
+contador_cliques = 0
+pausa = False
 def Compara_Cor(carta, cor):
-    carta.configure(fg_color=cor)  # Mostra a cor verdadeira da carta
-    # OBS.: Retire o 'global'
-    global numero_de_Tentativas, nome_carta1, cont, nome_carta2, cor1, cor2
 
-    if cont == 1:
-        cor1 = cor
-        nome_carta1 = carta
-        cont = 2
+    global pausa, contador_cliques, numero_de_Tentativas, nome_carta1, cont, nome_carta2, cor1, cor2   # OBS.: Retire o 'global'
 
-        print('cor 1', cor1)
-    elif cont == 2:
-        cor2 = cor
-        nome_carta2 = carta
-        cont = 1
+    contador_cliques += 1
+    print('Nome da carta: ', carta)
 
-        print('cor 2', cor2)
 
-        print('nome carta 1', nome_carta1, ' nome carta 2', nome_carta2)
-        if nome_carta1 != nome_carta2:
-            if cor1 == cor2:
-                # Esquecer as cartas iguais:
-                nome_carta1.after(1500, lambda: nome_carta1.place_forget())
-                nome_carta2.after(1500, lambda: nome_carta2.place_forget())
+    if pausa == True:
+        contador_cliques = 1
+        sleep(4)
+        pausa = False
 
-                print(f"A cor '{cor1}' é igual a cor '{cor2}'")
-            else:
-                print(f"A cor '{cor1}' é diferente da cor '{cor2}'")
+    print("contador_cliques", contador_cliques)#
 
-            # Retorna as cartas para a cor preto:
-            nome_carta1.after(
-                3000, lambda: nome_carta1.configure(fg_color="Black"))
-            nome_carta2.after(
-                3000, lambda: nome_carta2.configure(fg_color="Black"))
-        else:
-            pass
+    if 1 <= contador_cliques <= 2:
+        carta.configure(fg_color=cor)  # Mostra a cor verdadeira da carta
 
-        # Conta a quantidade de tentativas
-        numero_de_Tentativas += 1
-        tentativas.configure(text=f"Tentativas: {numero_de_Tentativas}/25")
+        if cont == 1:
+            cor1 = cor
+            nome_carta1 = carta
+            cont = 2
 
-        if numero_de_Tentativas >= 25:
-            finished = ctk.CTkLabel(
-                janela, text='Ops: Quantidade de tentativas excedida \n Reinicie o jogo 😉', font=('arial bold', 20))
-            finished.place(x=100, y=200)
+            print('cor 1', cor1)
+        elif cont == 2:
+            cor2 = cor
+            nome_carta2 = carta
+            cont = 1
 
+            pausa = True
+            print('cor 2', cor2)
+            print('nome carta 1', nome_carta1, ' nome carta 2', nome_carta2)
+
+            if nome_carta1 != nome_carta2:
+                if cor1 == cor2:
+                    # Esquecer as cartas iguais:
+                    nome_carta1.after(2000, lambda: nome_carta1.place_forget())
+                    nome_carta2.after(2000, lambda: nome_carta2.place_forget())
+
+                    print(f"A cor '{cor1}' é igual a cor '{cor2}'")
+                else:
+                    print(f"A cor '{cor1}' é diferente da cor '{cor2}'")
+
+                # Retorna as cartas para a cor preto:
+                nome_carta1.after(
+                    2000, lambda: nome_carta1.configure(fg_color="Black"))
+                nome_carta2.after(
+                    2000, lambda: nome_carta2.configure(fg_color="Black"))
+
+            # Conta a quantidade de tentativas
+            numero_de_Tentativas += 1
+            tentativas.configure(text=f"Tentativas: {numero_de_Tentativas}/25")
+
+            #Mostra a quantidade de tentativas atualizada:
+            if numero_de_Tentativas >= 25:
+                finished = ctk.CTkLabel(
+                    janela, text='Ops: Quantidade de tentativas excedida \n Reinicie o jogo 😉', font=('arial bold', 20))
+                finished.place(x=100, y=200)
+    
 
 # CONFIGURANDO A JANELA PRINCIPAL:
 janela = ctk.CTk()  # Criar janela
@@ -88,6 +105,8 @@ centralizar_janela(janela)
 
 # Criando cartas:
 cartas = []
+
+
 for linha in range(NUM_LINHAS):
     for coluna in range(NUM_COLUNAS):
         # nome_carta = f'carta{linha+1}x{coluna+1}'
@@ -97,7 +116,7 @@ for linha in range(NUM_LINHAS):
             text=f"CARTA {index+1}",
             width=100,
             height=120,
-            fg_color=str(CORES[index]),
+            fg_color='black',
             command=lambda i=index: Compara_Cor(cartas[i], str(CORES[i]))
         )
         carta.place(x=20+(coluna*110), y=20+(linha*130))
